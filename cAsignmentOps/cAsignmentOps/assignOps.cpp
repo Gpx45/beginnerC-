79e6b40af0f507b58c938e34a532eccc0dfaaf6c
Victor <vich.martins@gmail.com>
2016-09-20 15:05:47 -0400

    Add files via upload

---
+++ cAsignmentOps/cAsignmentOps/assignOps.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+
+	double b, x, y, z, a, c;
+
+	c, b, x, y, z, a, c = 100;
+	x += 5;		
+	y -= 2;		
+	z *= 10;	
+	a /= b;		
+	c %= 3;
+
+	cout << b << x << y << z << a << c;
+
+
+	return 0;
+}
